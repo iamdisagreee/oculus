3a5943bdac6e531df3ecafd08bb8ab8c30e206f7
vova <vavakharitonov2004@gmail.com>
2025-09-28 23:13:04 +0300

    Add adaptive layout

--- js/main.js
+++ js/main.js
@@ -24,3 +24,12 @@ function open(event){
     tabTarget.classList.add('tabs__btn-item--active')
     document.querySelector(`#${button}`).classList.add('tabs__content-item--active')
 }
+
+const menuBtn = document.querySelector('.menu-btn')
+const menu = document.querySelector('.menu')
+
+menuBtn.addEventListener('click', () => {
+    console.log('aue')
+    menu.classList.toggle('menu--active')
+    console.log(menu.classList)
+})
